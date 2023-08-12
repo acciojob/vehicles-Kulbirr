dfaf73a104241058db8a021e714c0fa61afd40fc
Kulbir Singh Bhandari <kulbirbhandari04@gmail.com>
2023-08-12 20:23:16 +0530

    Testing 3

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -24,35 +24,25 @@ public class F1 extends Car {
             changeGear(1);
             //Stop the car, set gear as 1
         }
-        else if(newSpeed > 0 && newSpeed <= 50){
+        else if (newSpeed <= 50) {
             changeGear(1);
-        }else if(newSpeed > 50 && newSpeed <= 100){
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
+        } else if (newSpeed <= 100) {
             changeGear(2);
-        }else if(newSpeed > 100 && newSpeed <= 150){
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
+        } else if (newSpeed <= 150) {
             changeGear(3);
-        }else if(newSpeed > 150 && newSpeed <= 200){
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
+        } else if (newSpeed <= 200) {
             changeGear(4);
-        }else if(newSpeed > 200 && newSpeed <= 250){
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
+        } else if (newSpeed <= 250) {
             changeGear(5);
-        }else {
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
+        } else {
             changeGear(6);
+            changeSpeed(newSpeed, getCurrentDirection()); // Update speed for all cases
         }
         //for all other cases, change the gear accordingly
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
-        else if(newSpeed > 0 && newSpeed <= 50){
-            changeSpeed(newSpeed, getCurrentDirection());
-        }else if(newSpeed > 50 && newSpeed <= 100){
-            changeSpeed(newSpeed, getCurrentDirection());
-        }else if(newSpeed > 100 && newSpeed <= 150){
-            changeSpeed(newSpeed, getCurrentDirection());
-        }else if(newSpeed > 150 && newSpeed <= 200){
-            changeSpeed(newSpeed, getCurrentDirection());
-        }else if(newSpeed > 200 && newSpeed <= 250){
-            changeSpeed(newSpeed, getCurrentDirection());
-        }else {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
     }
 }
